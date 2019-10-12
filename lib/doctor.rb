23e3a2a62b9afc014d181d5fcff3a82cd84dcd56
Emily Harber <emily.a.harber@gmail.com>
2019-10-12 21:53:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,7 +20,7 @@ class Doctor
   end 
   
   def patients 
-    Appointment.all.select { |appointment| appointment.patient if appointment.doctor == self }
+    Appointment.all.select { |appointment| appointment if appointment.doctor == self }
   end 
   
 end 
